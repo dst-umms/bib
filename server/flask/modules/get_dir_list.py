6a8b3806f41aab8e27fb29b61c49da742d5b59b4
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-10-12 16:37:27 -0400

    added path to each node in the tree

--- server/flask/modules/get_dir_list.py
+++ server/flask/modules/get_dir_list.py
@@ -7,12 +7,12 @@ import json
 
 def path_to_dict(path, id_num):
     if os.path.isdir(path):
-        node_obj = {'id': id_num, 'name': os.path.basename(path), 'children': []}
+        node_obj = {'id': id_num, 'name': os.path.basename(path), 'path': path, 'children': []}
         for cur_dir in os.listdir(path):
             id_num = id_num + 1
             node_obj['children'].append(path_to_dict(os.path.join(path,cur_dir), id_num))
     else:
-        node_obj = {'id': id_num, 'name': os.path.basename(path)}
+        node_obj = {'id': id_num, 'name': os.path.basename(path), 'path': path}
         id_num = id_num + 1
     return node_obj
 
