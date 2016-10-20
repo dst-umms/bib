0b8427ae69fbddfd82d8c458c048ee5264a2805c
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-10-20 15:11:01 -0400

    script to run all servers

---
+++ run_servers.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+flask run --port 7999 >&flask.log &
+server/express/node_modules/nodemon/bin/nodemon.js server/express/server.js >&express.log &
+ng serve --port 8000 --proxy-config proxy.conf.json >&client.log &
+
+wait
+
