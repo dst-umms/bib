87aac8dd7b16a19b02a8d71531e399fff8d0f53d
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-10-21 12:09:40 -0400

    added host flag to flask and ng servers

--- run_servers.bash
+++ run_servers.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-flask run --port 7999 >&flask.log &
+flask run --host "0.0.0.0" --port 7999 >&flask.log &
 ./server/express/node_modules/nodemon/bin/nodemon.js server/express/server.js >&express.log &
-ng serve --port 8000 --proxy-config proxy.conf.json >&client.log &
+ng serve --prod --host 0.0.0.0 --port 8000 --proxy-config proxy.conf.json >&client.log &
 
 wait
 
